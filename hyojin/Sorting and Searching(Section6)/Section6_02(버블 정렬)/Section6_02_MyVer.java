b6a39e94a924ec76aaf168ee768e63fe60e79d82
imhyojin <gywls5184@naver.com>
2024-04-03 02:41:21 +0900

    Section6(Sorting and Searching) 2.버블 정렬

---
+++ hyojin/Sorting and Searching(Section6)/Section6_02(버블 정렬)/Section6_02_MyVer.java
@@ -0,0 +1,29 @@
+package Section6_02;
+
+import java.util.Scanner;
+
+public class Section6_02_MyVer {
+    public int[] solution(int n, int[] arr) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Section6_02_MyVer t = new Section6_02_MyVer();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        for (int x : t.solution(n, arr)) System.out.print(x + " ");
+    }
+}
