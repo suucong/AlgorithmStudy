08075b0d639b21e502f319ec23a1e3c8141072ee
hyojin425 <gywls5184@naver.com>
2024-04-18 19:39:05 +0900

    Section7(Recursive, Tree, Graph) 3.팩토리얼

---
+++ hyojin/Recursive, Tree, Graph(Section7)/Section7_03(팩토리얼)/Section7_03_MyVer.java
@@ -0,0 +1,17 @@
+package Section7_03;
+
+import java.util.Scanner;
+
+public class Section7_03_MyVer {
+    public static int DFS(int n) {
+        if(n == 1) return 1;
+        else{
+            return n * DFS(n-1);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(DFS(n));
+    }
+}
